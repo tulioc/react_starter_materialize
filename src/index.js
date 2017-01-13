a9143dc8242a0262696dad42e50fe38219c7802f
Tulio Coelho <tulioopc@gmail.com>
2017-01-13 03:05:55 -0200

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Maarten on 24-2-2016.
+ */
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Hello from './components/helloworld.js'
+
+
+
+const App = ()=>{
+    return(
+        <div>
+            <Hello />
+        </div>
+    );
+};
+
+
+//Take the component generated HTM and put it om the page
+ReactDOM.render(<App/>, document.getElementById('container'));
